1a6c90b4b1caa43787284c67b47698d2c55d88fc
Ana Almeida <anaclaudia.rejei@hotmail.com>
2025-01-28 12:50:14 -0300

    Organizando a nova estrutura

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,11 @@
 import React from "react";
 import "../Header/styles.scss";
-import Logo from "../../assets/png/logotipo-econverse.png";
-import Lupa from "../../assets/svg/icon-lupa-busca.svg";
-import Pedidos from "../../assets/svg/icon-pedidos.svg";
-import Favoritos from "../../assets/svg/icon-favoritos.svg";
-import MinhaConta from "../../assets/svg/icon-usuario.svg";
-import Carrinho from "../../assets/svg/icon-carrinho-de-compras.svg";
 
 const Header: React.FC = () => {
   return (
     <div className="header">
-      <img className="logo" src={Logo} alt="Logotipo Econverse" />
-      <div className="search">
-        <input className="placeholder-search" type="text" placeholder="O que você está buscando?" />
-        <img className="lens" src={Lupa} alt="Ícone Lupa" />
-      </div>
-      <div className="icons-header">
-        <img src={Pedidos} alt="Ícone Pedidos" />
-        <img src={Favoritos} alt="Ícone Favoritos" />
-        <img src={MinhaConta} alt="Ícone Minha Conta" />
-        <img src={Carrinho} alt="Ícone Carrinho" />
-      </div>
-    </div>  
+
+    </div>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.scss";
-import TopBar from "./components/TopBar";
-import Header from "./components/Header";
-import NavBar from "./components/NavBar";
-import BannerHome from "./components/BannerHome";
-import CategorySlider from "./components/CategorySlider";
-import BannerGrid from "./components/BannerGrid";
-import BrandSlider from "./components/BrandsSlider";
-import Newsletter from "./components/Newsletter";
-import Footer from "./components/Footer";
-import ShelfProducts from "./components/ShelfProducts";
-import FirstShelfProducts from "./components/FirstShelfProducts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TopBar />
-    <Header />
-    <NavBar />
-    <BannerHome />
-    <CategorySlider />
-    <FirstShelfProducts />
-    <BannerGrid />
-    <ShelfProducts />
-    <BannerGrid />
-    <BrandSlider />
-    <ShelfProducts />
-    <Newsletter />
-    <Footer />
+
   </StrictMode>
 );
